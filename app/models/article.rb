3229ff230147d5b68aeba684316b615956c126fd
Линберг Даня <hhappyzor@gmail.com>
2022-07-14 16:06:47 +0300

    cascade destroy

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-    has_many :comments
+    has_many :comments, dependent: :destroy
     
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
